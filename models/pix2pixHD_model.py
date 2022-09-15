66fc5224d2d5f56680e80e4de2094345eb4213e7
Anonymous would rather not say <campbellrankine@gmail.com>
2022-09-15 23:04:44 +1000

    remove transforms for vector field

--- models/pix2pixHD_model.py
+++ models/pix2pixHD_model.py
@@ -142,6 +142,8 @@ class Pix2PixHDModel(BaseModel):
         return input_label, inst_map, real_image, feat_map
 
     def discriminate(self, input_label, test_image, use_pool=False):
+        print("input: " + str(input_label.shape))
+        print("test: " + str(test_image.detach().shape))
         input_concat = torch.cat((input_label, test_image.detach()), dim=1)
         if use_pool:            
             fake_query = self.fake_pool.query(input_concat)
