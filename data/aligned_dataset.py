c34a937b37e483b9f1bb1b2b40cae7209e33c41d
Anonymous would rather not say <campbellrankine@gmail.com>
2022-09-15 23:24:05 +1000

    remove dimension test10

--- data/aligned_dataset.py
+++ data/aligned_dataset.py
@@ -63,7 +63,6 @@ class AlignedDataset(BaseDataset):
         if self.opt.isTrain or self.opt.use_encoded_image:
             B_path = self.B_paths[index]   
             B = load_compressed_tensor(B_path)
-            print(B.shape)
             #transform_B = get_transform(self.opt, params)      
             B_tensor = F.interpolate(B, size=(720,1280))
             print(B_tensor.shape)
